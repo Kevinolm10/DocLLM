30359fa55125265daf2037ba863fd62bcb2be442
Kevin Ölmheden <kevinolmheden@MacBook-Air-som-tillhor-Kevin.local>
2025-11-04 15:11:04 +0100

    root

--- docllm/src/main.tsx
+++ docllm/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.tsx'
+import './styles/chatBoxContainer.css';
+import './styles/chatInterface.css';
+import './styles/sendButton.css';
+import './styles/chatInput.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
